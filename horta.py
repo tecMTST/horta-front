6886664ee8b63fd8709453a233e3378e92532ce9
dsilvadori <dev.maquinads@gmail.com>
2021-07-29 18:49:53 -0300

    Implementa variável para a temperatura

--- horta.py
+++ horta.py
@@ -8,8 +8,8 @@ app = Flask(__name__, template_folder='./templates')
 def horta():
     dados = requests.get('https://api.thingspeak.com/channels/1441189/feeds.json?results=1').json()
     u_solo = dados['feeds'][0]['field1']
-    t_ar = dados['feeds'][0]['field2']
+    t_ar = dados['feeds'][0]['field5']
     u_ar = dados['feeds'][0]['field3']
-    return render_template('/index.html', umidade_do_solo=u_solo, temperatura_do_ar=t_ar, umidade_do_ar=u_ar)
+    return render_template('/index.html', umidade_do_solo=u_solo, temperatura_do_ar= t_ar, perc_temperatura=float(t_ar)*2, umidade_do_ar=u_ar)
 
 app.run()
